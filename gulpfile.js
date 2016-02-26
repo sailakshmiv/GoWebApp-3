4d03db19408e5217b2225b4d494d8f15d3c7442a
yann Druffin <viking_du_sud@hotmail.com>
2016-02-26 14:54:26 -0800

    Added typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var ts = require('gulp-typescript');
 
 var lessPath = './less/**/*.less';
+var typescriptPath = './typescript/**/*.ts'
 
 gulp.task('less', function () {
   return gulp.src('less/site.less')
@@ -12,6 +14,13 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./wwwroot/css'));
 });
 
+gulp.task('typescript', function() {
+  console.log('Compiling typescript');
+  return gulp.src([typescriptPath])
+    .pipe(ts({module: 'commonjs'})).js.pipe(gulp.dest('./wwwroot/js'))
+});
+
 gulp.task('watch', function() {
   gulp.watch(lessPath, ['less']);
+  gulp.watch(typescriptPath, ['typescript']);
 });
