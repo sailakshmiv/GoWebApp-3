f05bf5f40f98155a079ae26ef63f9a779166eed3
yann Druffin <viking_du_sud@hotmail.com>
2016-03-08 17:46:48 -0800

    login method

--- controllers/account.go
+++ controllers/account.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"errors"
 	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/yanndr/GoWebApp/security"
@@ -29,8 +31,39 @@ func (this *AccountController) Login(w http.ResponseWriter, r *http.Request) {
 
 func (this *AccountController) PostLogin(w http.ResponseWriter, r *http.Request) {
 
-	security.GetInstance().CreateCookie(w)
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if user, err := login(username, password); err == nil {
+		log.Println(user)
+		security.GetInstance().CreateCookie(w)
+		http.Redirect(w, r, "/", 302)
+	}
+
 	template := this.templates.Lookup("_layout.html")
 	template.ParseFiles("login.html")
 	template.Execute(w, nil)
+}
+
+type User struct {
+	Id   int
+	Name string
+}
+
+func login(username string, password string) (*User, error) {
+
+	//hashedPassword := sha512.Sum512([]byte(password))
+	//b64Pass := base64.StdEncoding.EncodeToString(hashedPassword[:])
+	log.Println(username)
+	log.Println(password)
+
+	if username == "yann" && password == "password" {
+		log.Println("Login Ok")
+		result := User{Id: 1, Name: "Yann"}
+		return &result, nil
+	} else {
+		log.Println("Login Failed")
+		return nil, errors.New("Nope")
+	}
+
 }
